ee9f2affe3bf2524c1402ec3cd57c5875a9b5611
Lê Dũng <ledung.itsme@gmail.com>
2021-07-31 11:55:00 +0700

    Init

---
+++ ImGui DirectX 9 Kiero Hook 2/main.cpp
@@ -0,0 +1,151 @@
+#include "includes.h"
+
+#include "csgo.hpp"
+
+using namespace hazedumper::netvars;
+using namespace hazedumper::signatures;
+
+#ifdef _WIN64
+#define GWL_WNDPROC GWLP_WNDPROC
+#endif
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+EndScene oEndScene = NULL;
+WNDPROC oWndProc;
+static HWND window = NULL;
+
+void InitImGui(LPDIRECT3DDEVICE9 pDevice)
+{
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags = ImGuiConfigFlags_NoMouseCursorChange;
+	ImGui_ImplWin32_Init(window);
+	ImGui_ImplDX9_Init(pDevice);
+}
+
+bool init = false;
+
+bool show_menu = true;
+
+bool esp = false;
+
+bool bhop = false;
+
+long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice)
+{
+	if (!init)
+	{
+		InitImGui(pDevice);
+		init = true;
+	}
+
+	if (GetAsyncKeyState(VK_INSERT) & 1)
+	{
+		show_menu = !show_menu;
+	}
+
+	ImGui_ImplDX9_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+
+	if (show_menu)
+	{
+		ImGui::Begin("Le Dung Hacking");
+		ImGui::Checkbox("ESP", &esp);
+		ImGui::Checkbox("BHOP", &bhop);
+		ImGui::End();
+	}	
+
+	ImGui::EndFrame();
+	ImGui::Render();
+	ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+
+	return oEndScene(pDevice);
+}
+
+LRESULT __stdcall WndProc(const HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+
+	if (true && ImGui_ImplWin32_WndProcHandler(hWnd, uMsg, wParam, lParam))
+		return true;
+
+	return CallWindowProc(oWndProc, hWnd, uMsg, wParam, lParam);
+}
+
+BOOL CALLBACK EnumWindowsCallback(HWND handle, LPARAM lParam)
+{
+	DWORD wndProcId;
+	GetWindowThreadProcessId(handle, &wndProcId);
+
+	if (GetCurrentProcessId() != wndProcId)
+		return TRUE; // skip to next window
+
+	window = handle;
+	return FALSE; // window found abort search
+}
+
+HWND GetProcessWindow()
+{
+	window = NULL;
+	EnumWindows(EnumWindowsCallback, NULL);
+	return window;
+}
+
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	bool attached = false;
+	do
+	{
+		if (kiero::init(kiero::RenderType::D3D9) == kiero::Status::Success)
+		{
+			kiero::bind(42, (void**)& oEndScene, hkEndScene);
+			do
+				window = GetProcessWindow();
+			while (window == NULL);
+			oWndProc = (WNDPROC)SetWindowLongPtr(window, GWL_WNDPROC, (LONG_PTR)WndProc);
+			attached = true;
+		}
+	} while (!attached);
+	return TRUE;
+}
+
+DWORD WINAPI BhopThread(LPVOID lp)
+{
+	DWORD gameModule = (DWORD)GetModuleHandle("client.dll");
+	DWORD localPlayer = *(DWORD*)(gameModule + dwLocalPlayer);
+
+	while (localPlayer == NULL)
+	{
+		localPlayer = *(DWORD*)(gameModule + dwLocalPlayer);
+	}
+
+	while (true)
+	{
+		if (bhop)
+		{
+			DWORD flag = *(BYTE*)(localPlayer + m_fFlags);
+			if (GetAsyncKeyState(VK_SPACE) && flag && (1 << 0))
+			{
+				*(DWORD*)(gameModule + dwForceJump) = 6;
+			}
+		}
+	}
+}
+
+
+
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		CreateThread(nullptr, 0, BhopThread, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		kiero::shutdown();
+		break;
+	}
+	return TRUE;
+}
